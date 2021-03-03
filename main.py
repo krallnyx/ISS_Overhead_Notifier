f18a906e953fb2f238de8988cb24b490666bc1f0
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-03-03 13:47:47 +0000

    #1 App correctly able to send email, need to be tested at nightime and when ISS is in the sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime
+import smtplib
+from email.message import EmailMessage
 
 MY_LAT = 51.4014  # Your latitude
 MY_LONG = 1.3231  # Your longitude
@@ -14,28 +16,42 @@ class ISSOverhead:
         }
         self.iss_latitude = 0
         self.iss_longitude = 0
-        self.time_now = 0
-        self.sunrise = 0
-        self.sunset = 0
+        self.time_now = 0.0
+        # time_now, sunrise and sunset are used as floats, minutes aren't base 100 but still based 60 so it's not
+        # meant to be used !
+        # it just doesn't matter in our case as it's only used to compare values
+        self.sunrise = 0.0
+        self.sunset = 0.0
+        self.smtp_server = "smtp.email.com"
+        self.email = "email@email.com"
+        self.password = ""
+        self.msg = EmailMessage()
 
     def check_night(self):
-        if int(self.sunrise[0]) <= self.time_now.hour:
-            if int(self.sunrise[1]) <= self.time_now.minute:
-                print("the sun has already risen today")
-                if int(self.sunset[0]) >= self.time_now.hour:
-                    if int(self.sunset[1]) >= self.time_now.minute:
-                        # the sun hasn't set yet, it's daytime
-                        return True
-        return False
+        if self.sunrise < self.time_now < self.sunset:
+            print("It's daytime")
+            return False
+        return True
 
     def check_iss_in_sky(self):
+        print(f"my latitude: {MY_LAT}, ISS latitude: {self.iss_latitude}")
+        print(f"my longitude: {MY_LONG}, ISS longitude: {self.iss_longitude}")
         if MY_LAT+5 > self.iss_latitude > MY_LAT-5:
             if MY_LONG+5 > self.iss_longitude > MY_LONG-5:
+                print("ISS in the sky")
                 return True
         return False
 
     def send_email(self):
-        pass
+        self.msg.set_content("The International Space Station is currently visible in your night sky!\nLook up!")
+        self.msg['Subject'] = "ISS visible"
+        self.msg['From'] = self.email
+        self.msg['To'] = self.email
+        with smtplib.SMTP(self.smtp_server) as connection:
+            connection.starttls()
+            connection.login(user=self.email, password=self.password)
+            connection.send_message(self.msg)
+        print("Email sent")
 
     def request_apis(self):
 
@@ -52,16 +68,17 @@ class ISSOverhead:
         response = requests.get("https://api.sunrise-sunset.org/json", params=self.parameters)
         response.raise_for_status()
         data = response.json()
-        self.sunrise = data["results"]["sunrise"].split("T")[1].split(":")[:2]
-        self.sunset = data["results"]["sunset"].split("T")[1].split(":")[:2]
+        # as said in the __init__ method, we build WRONG times, using a flot with minutes still base 60
+        # it doesn't matter for us as it's only used to compare them
+        self.sunrise = float(f'{data["results"]["sunrise"].split("T")[1].split(":")[0]}.'
+                             f'{data["results"]["sunrise"].split("T")[1].split(":")[1]}')
+        self.sunset = float(f'{data["results"]["sunset"].split("T")[1].split(":")[0]}.'
+                            f'{data["results"]["sunset"].split("T")[1].split(":")[1]}')
 
-        self.time_now = datetime.now()
+        self.time_now = float(f'{datetime.now().hour}.{datetime.now().minute}')
 
         night = self.check_night()
 
-        # If the ISS is close to my current position
-        # and it is currently dark
-        # Then send me an email to tell me to look up.
         if night and in_sky:
             self.send_email()
 
